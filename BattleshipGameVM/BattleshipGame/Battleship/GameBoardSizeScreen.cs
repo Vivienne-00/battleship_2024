0ceb85eac5965ea29cb42c54e53b2b01326c69c5
Vivienne Gschwend <viviennegschwend@gmail.com>
2024-01-29 22:06:01 +0100

    GameBoardSizeScreen erstellt

---
+++ BattleshipGameVM/BattleshipGame/Battleship/GameBoardSizeScreen.cs
@@ -0,0 +1,15 @@
+﻿namespace Battleship
+{
+    public partial class GameBoardSizeScreen : Form
+    {
+        public GameBoardSizeScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
