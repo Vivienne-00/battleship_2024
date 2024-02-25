3263f246f554ff435157c3c945b5ee1a55c517f3
EMCninja3 <Emcninja333@gmail.com>
2024-02-25 18:58:35 +0100

    P2 state

--- BattleshipGameVM/BattleshipGame/Battleship/Model/StateMachine/Players2TurnState.cs
+++ BattleshipGameVM/BattleshipGame/Battleship/Model/StateMachine/Players2TurnState.cs
@@ -1,26 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 
-namespace Battleship.Model.StateMachine {
-    public class Players2TurnState : PlayersTurnState {
-        public override void EnterState(BattleshipGame game) {
+namespace Battleship.Model.StateMachine
+{
+    public class Players2TurnState : PlayersTurnState
+    {
+        public override void EnterState(BattleshipGame game)
+        {
             base.EnterState(game);
             Debug.WriteLine("Enter Players2TurnState");
         }
 
-        public override void HandleInput(BattleshipGame game, Coordinate coordinate) {
+        public override void HandleInput(BattleshipGame game, Coordinate coordinate)
+        {
             // Input behandeln (Getroffen, ja/nein)
 
             // entsprechend State ändern
-
-            if (coordinate.X == 2 && coordinate.Y == 2) {
+            Console.WriteLine("Player 2 is shoooting");
+            if (coordinate.X == 2 && coordinate.Y == 2)
+            {
                 // Treffer
                 game.TransitionToState(new Players2TurnState());
-            } else {
+            }
+            else
+            {
                 game.TransitionToState(new Players1TurnState());
 
             }
