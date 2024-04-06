88ecae28b175e6dd4415575d107774f4c1e2b810
EMCninja3 <Emcninja333@gmail.com>
2024-04-06 04:57:51 +0200

    SplashScreen wechselt automatisch zum nächsten Screen

--- BattleshipGameVM/BattleshipGame/Battleship/SplashScreen.Designer.cs
+++ BattleshipGameVM/BattleshipGame/Battleship/SplashScreen.Designer.cs
@@ -20,45 +20,33 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+		#region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            button1 = new Button();
-            SuspendLayout();
-            // 
-            // button1
-            // 
-            button1.Location = new Point(295, 423);
-            button1.Margin = new Padding(3, 4, 3, 4);
-            button1.Name = "button1";
-            button1.Size = new Size(344, 145);
-            button1.TabIndex = 0;
-            button1.Text = "Start";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
-            // 
-            // SplashScreen
-            // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
-            AutoScaleMode = AutoScaleMode.Font;
-            BackgroundImage = Properties.Resources.BGSplashScreen;
-            BackgroundImageLayout = ImageLayout.Stretch;
-            ClientSize = new Size(914, 600);
-            Controls.Add(button1);
-            Margin = new Padding(3, 4, 3, 4);
-            Name = "SplashScreen";
-            Text = "BattleShip by Vivienne G., Miguel F.";
-            Load += SplashScreen_Load;
-            ResumeLayout(false);
-        }
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			SuspendLayout();
+			// 
+			// SplashScreen
+			// 
+			AutoScaleDimensions = new SizeF(8F, 20F);
+			AutoScaleMode = AutoScaleMode.Font;
+			BackgroundImage = Properties.Resources.BGSplashScreen;
+			BackgroundImageLayout = ImageLayout.Stretch;
+			ClientSize = new Size(914, 600);
+			Margin = new Padding(3, 4, 3, 4);
+			Name = "SplashScreen";
+			Text = "BattleShip by Vivienne G., Miguel F.";
+			Load += SplashScreen_Load;
+			Shown += SplashScreen_Shown;
+			ResumeLayout(false);
+		}
 
-        #endregion
+		#endregion
 
-        private Button button1;
+		private Button button1;
     }
 }
